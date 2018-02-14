f1969b073cd3232667bb89e7a29e499f70e77f8c
Marco Conti <marcoconti2010@gmail.com>
2018-02-14 23:11:59 +0100

    Here we go

---
+++ README.md
@@ -0,0 +1,113 @@
+# SSHKit
+
+[![license](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![goreportcard](https://goreportcard.com/badge/github.com/mrccnt/sshkit)](https://goreportcard.com/report/github.com/mrccnt/sshkit)
+
+This package encapsulates my most common SSH actions needed in daily business. It simplifies creating/handling of SSH
+connections, simple SSH tunnels or SFTP filetransfers. Additionally it adds `AgentAuth` as `ssh.AuthMethod`. This way
+you can use your local SSH agent for authentication and do not have to handle passwords.
+
+## Examples
+
+Keep in mind that these are examples. Handle your errors! Do not panic! ;)
+
+### SFTP
+
+Transfer a file via SFTP from remote server to locahost:
+
+```go
+package main
+
+import (
+	"github.com/mrccnt/sshkit"
+	"golang.org/x/crypto/ssh"
+	"os"
+)
+
+func main() {
+
+	sshClient, err := sshkit.SSHClient(
+		sshkit.SSHConfig(
+			"username",
+			[]ssh.AuthMethod{sshkit.AgentAuth()},
+		),
+		"domain.tld:22",
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer sshClient.Close()
+
+	sftpClient, err := sshkit.SFTPClient(sshClient)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer sftpClient.Close()
+
+	srcFile, err := sftpClient.Open("path/on/remote/to/file.md")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create("path/on/local/to/copy.md")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer dstFile.Close()
+
+	_, err = srcFile.WriteTo(dstFile)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+```
+
+### Tunnel
+
+Create a tunnel to a remote host to make remote mysql server available on localhost port 13306:
+
+```go
+package main
+
+import (
+    "github.com/mrccnt/sshkit"	
+    "golang.org/x/crypto/ssh"	
+)
+
+func main(){
+	// Create the complete tunnel configuration
+	// You need a ssh.ClientConfig and someEndpoints
+	tunnel := &sshkit.SSHTunnel{
+		// Create a SSH ClientConfiguration with your requirements.
+		Config: &ssh.ClientConfig{
+			User: "username",
+			Auth: []ssh.AuthMethod{
+				sshkit.AgentAuth(),
+			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		},
+		// The tunneled port from your remote server will be available on your localhost on port 13306
+		Local: &sshkit.Endpoint{
+			Host: "localhost",
+			Port: 13306,
+		},
+		// Your remote SSH server
+		Server: &sshkit.Endpoint{
+			Host: "domain.tld",
+			Port: 22,
+		},
+		// On your remote host we will use localhosts MySql server on default port 3306
+		Remote: &sshkit.Endpoint{
+			Host: "localhost",
+			Port: 3306,
+		},
+	}
+	// Initialize/Start the tunnel.
+	// Keep in mind: This is a blocking action...
+	err := tunnel.Start()
+	if err != nil {
+		panic(err.Error())
+	}
+}
+```
---
+++ sshkit.go
@@ -0,0 +1,130 @@
+// Copyright 2017 Marco Conti
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sshkit
+
+import (
+	"fmt"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+	"io"
+	"net"
+	"os"
+)
+
+type (
+	SSHTunnel struct {
+		Local  *Endpoint
+		Server *Endpoint
+		Remote *Endpoint
+		Config *ssh.ClientConfig
+	}
+	Endpoint struct {
+		Host string
+		Port int
+	}
+)
+
+// SSHConfig returns a ssh.ClientConfig for given parameters
+func SSHConfig(username string, auths []ssh.AuthMethod) *ssh.ClientConfig {
+	return &ssh.ClientConfig{
+		User:            username,
+		Auth:            auths,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+}
+
+// SSHClient returns a SSH Client for given config and address
+func SSHClient(sshCfg *ssh.ClientConfig, addr string) (*ssh.Client, error) {
+	c, err := ssh.Dial("tcp", addr, sshCfg)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// LocalAgent returns a connection to the local authentication agent via auth sock env variable
+func LocalAgent() (net.Conn, error) {
+	a, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+// AgentAuth returns a ssh.AuthMethod to use your local authentication agent
+func AgentAuth() ssh.AuthMethod {
+	a, err := LocalAgent()
+	if err != nil {
+		panic(err.Error())
+	}
+	return ssh.PublicKeysCallback(agent.NewClient(a).Signers)
+}
+
+// SFTPClient returns a client to handle any SFTP actions for given SSH Client
+func SFTPClient(sshClient *ssh.Client) (*sftp.Client, error) {
+	c, err := sftp.NewClient(sshClient)
+	if err != nil {
+		return nil, nil
+	}
+	return c, nil
+}
+
+// String returns the prefered ssh package address format
+func (endpoint *Endpoint) String() string {
+	return fmt.Sprintf("%s:%d", endpoint.Host, endpoint.Port)
+}
+
+// Start initializes the tunnel and starts a forwarding (blocking)
+func (tunnel *SSHTunnel) Start() error {
+	l, err := net.Listen("tcp", tunnel.Local.String())
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	for {
+		local, err := l.Accept()
+		if err != nil {
+			return err
+		}
+		go tunnel.forward(local)
+	}
+}
+
+// forward handles any incoming/outgoing reader/writer for this tunnel
+func (tunnel *SSHTunnel) forward(local net.Conn) {
+
+	server, err := ssh.Dial("tcp", tunnel.Server.String(), tunnel.Config)
+	if err != nil {
+		fmt.Printf("Server dial error: %s\n", err)
+		return
+	}
+
+	remote, err := server.Dial("tcp", tunnel.Remote.String())
+	if err != nil {
+		fmt.Printf("Remote dial error: %s\n", err)
+		return
+	}
+
+	copyConn := func(writer, reader net.Conn) {
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			fmt.Printf("io.Copy error: %s", err)
+		}
+	}
+
+	go copyConn(local, remote)
+	go copyConn(remote, local)
+}
